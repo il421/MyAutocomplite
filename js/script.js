4807d1b4d96e4f6c41c73dd4bdbf7b277ca07ae9
il421 <il421@mail.ru>
2017-03-09 23:02:43 +0300

    Filter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// LOAD DATA
 (function () {
   var CITIES_DATE = '../kladr.json';
   window.load = function (onload) {
@@ -18,35 +19,51 @@ window.load(function (evt) {
   var searchboxResult = document.querySelector('.city-options__result');
   var searchboxInput = document.querySelector('.city-searchbox input');
   var searchboxOptions = document.querySelector('.city-options');
+  var KEY_SPACE = 32;
 
-  // var renderItem = function (arr) {
-  //   var cityResult = document.querySelectorAll('.city-options__result span');
-  //   console.log(cityResult);
-  //   for (var i = 0; i < arr.length; i++) {
-  //     cityResult[i].textContent = arr[i].City;
-  //   }
+  var renderItem = function (arr) {
+    var cityResult = document.querySelectorAll('.city-options__result span');
+    for (var i = 0; i < arr.length; i++) {
+      cityResult[i].textContent = arr[i].City;
+    }
+  };
+
+  var dateFiltred = function (src) {
+    return data.filter(function (elm) {
+      return elm.City.toLowerCase().indexOf(src.toLowerCase()) > -1;
+    });
+  };
+
+  // var test = function () {
+  //   searchboxList.firstChild.style.background = '#1e90ff';
   // };
 
-  // data.forEach(function () {
-  //   searchboxList.appendChild(searchboxResult.cloneNode(true));
-  // });
+  // FILTER DATA
+  searchboxInput.addEventListener('keyup', function (evt) {
+    var searchValue = searchboxInput.value;
 
-  // renderItem(data);
-
-  searchboxInput.addEventListener('keyup', function () {
-    searchboxOptions.style.display = 'block';
-
-    for (var i = 0; i < 5000; i++) {
-      var dataCity = data[i].City;
-      var cityResult = document.querySelectorAll('.city-options__result span');
-
-      if (dataCity.indexOf(searchboxInput.value) > -1) {
-        searchboxList.appendChild(searchboxResult.cloneNode(cityResult[i].textContent = dataCity));
+    if (evt.keyCode !== KEY_SPACE) {
+      while (searchboxList.lastChild) {
+        searchboxList.removeChild(searchboxList.lastChild);
       }
+
+      dateFiltred(searchValue).forEach(function () {
+        searchboxList.appendChild(searchboxResult.cloneNode(true));
+      });
+
+      renderItem(dateFiltred(searchValue));
+
+      searchboxOptions.style.display = 'block';
     }
   });
 
+  // HIDE LIST
   document.addEventListener('click', function () {
     searchboxOptions.style.display = 'none';
   });
+
+  // SELECT DATA
+  searchboxList.addEventListener('click', function (e) {
+    searchboxInput.value = e.target.innerText;
+  }, true);
 });
