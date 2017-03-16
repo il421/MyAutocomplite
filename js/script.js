c91b61cd89f3a4b2b1859d088b4f22d7a6ad411a
Ilya Suglobov <il421@mail.ru>
2017-03-16 10:38:09 +0300

    Corrections

--- js/script.js
+++ js/script.js
@@ -1,41 +1,5 @@
 'use strict';
 
-// LOAD DATA
-(function () {
-  var CITIES_DATE = '../kladr.json';
-  var loaderRing = document.querySelector('.loader');
-  var errorInformation = document.querySelector('.error');
-  var errorUpdate = document.querySelector('.error__update');
-  var timerLoader = function () {
-    setInterval(function () {
-      loaderRing.style.display = 'none';
-    }, 1000);
-  };
-
-  window.load = function (onload) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener('load', onload);
-    xhr.open('GET', CITIES_DATE);
-
-    errorUpdate.addEventListener('click', function (evt) {
-      xhr.addEventListener('load', onload);
-      errorInformation.style.display = 'none';
-    });
-
-    xhr.addEventListener('error', function () {
-      errorInformation.style.display = 'flex';
-    });
-
-    xhr.addEventListener('timeout', function (evt) {
-      timerLoader(loaderRing.style.display = 'flex');
-    });
-    xhr.timeout = 500;
-
-    xhr.send();
-  };
-})();
-
 window.load(function (evt) {
   var data = JSON.parse(evt.target.response);
 
