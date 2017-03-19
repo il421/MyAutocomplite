4bcbe476f69f28fec14e0590b3454dd10d999c3e
il421 <il421@mail.ru>
2017-03-19 17:40:30 +0300

    Corrections

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ var gulp = require('gulp');
 var server = require('browser-sync').create();
 var less = require('gulp-less');
 var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var run = require('run-sequence');
+var del = require('del');
 
 gulp.task('style', function () {
   gulp.src('less/style.less')
@@ -22,15 +26,41 @@ gulp.task('style', function () {
         sort: true
       })
     ]))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
 });
 
 gulp.task('serve', function () {
   server.init({
-    server: '.',
+    server: 'build',
+    notify: false,
+    open: true,
+    ui: false
   });
 
   gulp.watch('less/**/*.less', ['style']);
   gulp.watch('*.html').on('change', server.reload);
+});
+
+gulp.task('build', function (Fn) {
+  run('clean', 'copy', 'style', Fn);
+});
+
+gulp.task('copy', function () {
+  return gulp.src([
+    'fonts/*.ttf',
+    'js/**',
+    '*.html',
+    '*.json'
+  ], {
+    base: '.'
+  })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function () {
+  return del('build');
 });
